a43d80d8c92b64954669ef169d4b4ea14326f1a0
Molnár <Zsolt.Molnar@fincon.eu>
2020-07-15 13:40:25 +0200

    chore(udemy-pics): delete files
    delete component files from src directory

--- unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID 3er7QKRTq1DHFDZJveMb-QTAXd1P_Rr87iLnPkbAKV4",
-  },
-})
